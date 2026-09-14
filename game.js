d1abb16a0eabd07a8e83a48aee323535a35805b2
arafathossainlimon11 <arafathossainlimon11@gmail.com>
2026-09-14 14:57:26 +0600

    Update game.js
    Adjusted player vertical position to avoid touch buttons.

--- game.js
+++ game.js
@@ -145,7 +145,7 @@ class CarRacingGame {
         this.laneWidth = this.roadWidth / this.lanes;
         this.roadOffset = 0;
 
-        // Player Setup (Positioned higher to avoid bottom touch buttons)
+        // Player Setup
         this.player = {
             x: 0,
             y: 0,
@@ -195,8 +195,7 @@ class CarRacingGame {
         this.roadX = (this.width - this.roadWidth) / 2;
         this.laneWidth = this.roadWidth / this.lanes;
 
-        // Keep Player Above Touch Buttons Area
-        this.player.y = this.height - 190;
+        this.player.y = this.height - 150;
         if (this.state !== 'PLAYING') {
             this.player.x = this.roadX + (this.roadWidth / 2) - (this.player.width / 2);
         }
@@ -310,7 +309,7 @@ class CarRacingGame {
         this.player.boostMeter = 100;
         
         this.player.x = this.roadX + (this.roadWidth / 2) - (this.player.width / 2);
-        this.player.y = this.height - 190; // Car positioned clear off the touch buttons
+        this.player.y = this.height - 150;
 
         this.traffics = [];
         this.coinsList = [];
@@ -520,7 +519,7 @@ class CarRacingGame {
             rect1.x + margin < rect2.x + rect2.width - margin &&
             rect1.x + rect1.width - margin > rect2.x + margin &&
             rect1.y + margin < rect2.y + rect2.height - margin &&
-            rect1.y + rect1.height - margin > rect2.y + margin
+            rect1.y + margin < rect2.y + rect2.height - margin
         );
     }
 
